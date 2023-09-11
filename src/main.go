1870bddeacaca619de53e1cf0a637921d8fc0db6
GodavR <mail.davidrinaldi@gmail.com>
2023-09-11 19:09:32 +0700

    penambahan comment

--- src/main.go
+++ src/main.go
@@ -1,7 +1,27 @@
+/* menjalankan dengan cara masuk ke dir dengan comment dengan perintah go run test.go
+perintah init pada awal project dengan go mod init <nama-project>Setiap file program harus memiliki package. Setiap project harus ada minimal
+satu file dengan nama package main . File yang ber-package main , akan
+dieksekusi pertama kali ketika program di jalankan.
+Cara menentukan package dengan menggunakan keyword package , berikut
+adalah contoh penulisannya.
+package <nama-package>
+package main */
+
 package main
 
 import "fmt"
 
+/* Keyword import digunakan untuk meng-import atau memasukan package lain
+ke dalam file program, agar isi dari package yang di-import bisa dimanfaatkan.
+Package fmt merupakan salah satu package bawaan yang disediakan oleh Go,
+isinya banyak fungsi untuk keperluan I/O yang berhubungan dengan text.
+Berikut adalah skema penulisan keyword import :
+import "<nama-package>"
+import "fmt" */
+
 func main() {
+	/*komentar code
+	menamppilkan pesan hello word */
 	fmt.Println("Hello, World!")
+	//fmt.Println("Hello, World!") baris ini tidak akan dieksekusi
 }
--- src/main_test.go
+++ src/main_test.go
@@ -1,3 +1,5 @@
+// menjalankan dengan cara masuk ke dir dengan comment dengan perintah go test main_test.go
+
 package main
 
 import "fmt"
